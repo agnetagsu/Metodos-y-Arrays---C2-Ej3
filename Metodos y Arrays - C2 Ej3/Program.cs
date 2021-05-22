2ef60fa948c9aa99099552b9bebf36db9e1cfd9e
Agneta <agnetagsu@yahoo.es>
2021-05-23 01:26:26 +0200

    Agregar archivos de proyecto.

---
+++ Metodos y Arrays - C2 Ej3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Metodos_y_Arrays___C2_Ej3
+{
+    class EsNumeroPrimo
+    {
+        public Boolean Esprimo(int numeroprimo)
+        {
+            Boolean primo = true;
+            int i = numeroprimo;
+
+            do
+            {
+                i = i-1;
+                if(numeroprimo%i==0)
+                {
+                    primo = false;
+                }
+            } while ((i > 2) & (primo));
+            return primo;
+        }
+        static void Main(string[] args)
+        {
+            int numero;
+            EsNumeroPrimo posiblePrimo = new EsNumeroPrimo();
+            Console.WriteLine("Introduce el numero para comprobar si es primo o no:");
+            numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(posiblePrimo.Esprimo(numero));
+
+        }
+    }
+}
